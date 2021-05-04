665694e78739be5c66dec5353219da9e3113725e
unknown <ishobhit3@gmail.com>
2021-05-04 12:16:04 +0530

    added the search functionality

--- public/javascripts/addons.js
+++ public/javascripts/addons.js
@@ -1,4 +1,5 @@
 
+
 // search for forms on the basis of names
 var search=document.querySelector('.search')
 var btn=document.querySelector('#searchBtn')
@@ -7,12 +8,50 @@ btn.addEventListener('click',()=>{
     search.classList.toggle('active');
     if(search.classList.contains('active')) input.focus()
 })
-
-// var query = { address: "Park Lane 38" };
-// dbo.collection("customers").find(query).toArray(function(err, result) {
-//   if (err) throw err;
-//   console.log(result);
+const camps=campgrounds.features ;
+showCamps(camps)
 
 input.addEventListener('change',e=>{
-    console.log(e.target.value) ;
+    const searchTerm=(e.target.value) ;
+    if(searchTerm===''){
+        showCamps(camps)
+    }
+    else {
+        const arr=camps.filter(e=>e.title===searchTerm)
+        if(arr.length==0) showCamps(camps)
+       else  showCamps(arr)
+    }
+
 })
+
+function showCamps(camp){
+    const hold=document.querySelector('.hold')
+    hold.innerHTML='' ;
+        for(c of camp){
+        const div=document.createElement('div') ;
+        div.classList.add('container')
+        div.classList.add('mb-5')
+        div.innerHTML=` <div class="row">
+        <div class="col-md-4">
+                <img class="img-thumbnail" src="${c.images[0].url}" alt="camp image">
+        </div>
+        <div class="col-md-8">
+            <h5>
+                ${c.title}
+            </h5>
+            <p>
+                ${c.description}
+            </p>
+            <p>
+                <small class="text-muted">
+                    ${c.location}
+                </small>
+            </p>
+            <a class="btn btn-primary" href="/campgrounds/${c._id}">
+                click here
+            </a>
+        </div>
+    </div>`
+    hold.append(div) ;
+            }
+}
