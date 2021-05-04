2aa115443cfa47a5cf99062184573e2805ab9591
unknown <ishobhit3@gmail.com>
2021-05-04 19:04:29 +0530

    deploy

--- index.js
+++ index.js
@@ -118,8 +118,7 @@ app.use((err, req, res, next) => {
     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
     res.status(statusCode).render('error', { err })
 })
-// process.env.PORT ||
-const port =  3000;
+const port=process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
